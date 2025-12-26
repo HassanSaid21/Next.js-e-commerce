955528f9dd1617bdd66af69e2cab4832b3849e97
Hassan Said <karimsaid214@gmail.com>
2025-12-26 08:26:18 +0200

    update nav

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 import { Bell, Home } from "lucide-react";
 import Logo from "./Logo";
 import ShoppingCartIcon from "./ShoppingCartIcon";
-import {  SignedIn, SignedOut, SignInButton  } from "@clerk/nextjs";
+// import {  SignedIn, SignedOut, SignInButton  } from "@clerk/nextjs";
 import ProfileButton from "./ProfileButton";
 
 const Navbar = () => {
@@ -19,13 +19,18 @@ const Navbar = () => {
         <Link href='/'><Home className="w-4 h-4 text-gray-500" /></Link>
         <Bell  className="w-4 h-4 text-gray-500"/>
       <ShoppingCartIcon />
-    <SignedOut>
+      
+          {//! span should be replaced with clerk
+            }
+      <span className="font-semibold  text-gray-900 cursor-pointer ">Sign in</span>
+
+    {/* <SignedOut>
 
       <SignInButton />
     </SignedOut>
     <SignedIn>
       <ProfileButton />
-    </SignedIn>
+    </SignedIn> */}
       </div>
     </nav>
   );
